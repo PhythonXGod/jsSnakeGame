498c28ae1699cbe79c8b04618b150d1b4a947a39
unknown <ernests.augusts@gmail.com>
2022-11-02 21:09:02 +0200

    second commit

---
+++ game.js
@@ -0,0 +1,7 @@
+let lastRenderTime = 0
+
+function main(currentTime) {
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+    window.requestAnimationFrame(main)
+    lastRenderTime = currentTime
+}
